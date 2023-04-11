760c8422d7f809600c37058cfb016e0b1a06c521
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2023-04-11 20:33:37 +0530

    Made line follower robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "main.h"
-#include "Car.h"
-#include "RCSensor.h"
-#include "LED.h"
 #include "Buttons.h"
-#include "SensorService.h"
+#include "Car.h"
+#include "LED.h"
 #include "RCSensor.h"
+#include "SensorService.h"
 
 int pin_m1_a[5] = {33, 35, 35, 35, 35};
 int pin_m1_b[5] = {35, 33, 33, 33, 33};
@@ -22,14 +21,13 @@ int pin_led2 = A2;
 int pin_led3 = A0;
 int pin_battery = A9;
 uint16_t sensorValues[11] = {0};
-Car car = Car(pin_m1_a[BOT_ID], pin_m1_b[BOT_ID], pwm_m1, pin_m2_a[BOT_ID], pin_m2_b[BOT_ID], pwm_m2, standby, false);
-RCSensor rcsensors = RCSensor((const int[]){
-                                  30, 28, 26, 24, 22, 21, 23, 25, 27, 29, 31},
-                              false);
+Car car = Car(pin_m1_a[BOT_ID], pin_m1_b[BOT_ID], pwm_m1, pin_m2_a[BOT_ID],
+              pin_m2_b[BOT_ID], pwm_m2, standby, false);
+RCSensor rcsensors =
+    RCSensor((const int[]){30, 28, 26, 24, 22, 21, 23, 25, 27, 29, 31}, false);
 SensorService sensorService = SensorService();
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once
   delay(100);
   LED::init(pin_led1, pin_led2, pin_led3);
@@ -44,17 +42,19 @@ void setup()
   Serial.println("Press 1 for Line follower");
   Serial.println("Press 2 for wall follower");
   car.setMotorPower(true);
-  if (Buttons::getNextButtonPressed() == 2)
-  {
-    //code to follow wall
-  }
-  else
-  {
-    // code to follow line
-  }
 }
 
-void loop()
-{
-  //loop task
+void loop() {
+  // rcsensors.readSensors(sensorValues);
+  rcsensors.readCalibrateSensors(sensorValues);
+  int intensity = rcsensors.readLinePrivate(sensorValues, false);
+  if (intensity < 1500) {
+    car.setSpeeds(150, 0);
+  } else if (intensity > 2500) {
+    car.setSpeeds(0, 150);
+  } else {
+    car.setSpeeds(150, 150);
+  }
+  Serial.print(intensity);
+  Serial.println("");
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,6 @@
 #ifndef SRC_MAIN_H_
 #define SRC_MAIN_H_
 #include <Arduino.h>
-#define BOT_ID 1
+#define BOT_ID 3
 
 #endif /* SRC_MAIN_H_ */
